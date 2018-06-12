e7f546f4cf941c39e0ca25c8d98e3203826f2178
AAKASH HANDA <AAKASH HANDA>
2018-06-12 21:48:43 +0530

    A

--- Template form/src/app/app.component.ts
+++ Template form/src/app/app.component.ts
@@ -1,6 +1,7 @@
 import {Component} from '@angular/core';
 import { Employee } from './models/employee.model'
-
+import {FormPoster} from './service/form.poster';
+import { NgForm } from '@angular/forms'
 
 @Component({
   selector:'app-form',
@@ -9,14 +10,29 @@ import { Employee } from './models/employee.model'
 
 export class AppComponent{
   languages=["NodeJs","ReactJs","AngularJS"]
-  model = new Employee("John","Smith",true,"male","NodeJs")
+  model = new Employee("John","Smith","a@a.com","abcdef",true,"male","NodeJs");
+  hasCodeLang:boolean=false;
 
+  constructor(private _formposter:FormPoster){}
   firstToUpper(value:string){
     if(value.length>0){
       this.model.fname = value.charAt(0).toUpperCase()+value.slice(1)
     }
     else{
       this.model.fname = value
+    }
+  }
+
+  submitForm(form:NgForm){
+    this._formposter.postEmployeeForm(this.model)
+      .subscribe((data)=>console.log('success',data),
+        (err)=> console.log('error', err))
+  }
+  validateCodeLang(event):void{
+    if(this.model.codeLang === "default"){
+      this.hasCodeLang = true;
+    }else{
+      this.hasCodeLang=false
     }
   }
 }
--- Template form/src/app/models/employee.model.ts
+++ Template form/src/app/models/employee.model.ts
@@ -2,6 +2,8 @@ export class Employee{
     constructor(
         public fname:string,
         public lname:string,
+        public email:string,
+        public password:string,
         public fulltime:boolean,
         public gender:string,
         public codeLang:string
